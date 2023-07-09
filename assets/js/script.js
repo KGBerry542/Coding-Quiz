7a331ef4633d0d9fab90093c22fd694496b8df8b
KGBerry542 <74325710+KGBerry542@users.noreply.github.com>
2023-07-09 19:52:24 -0400

    adding quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+// Quiz questions
+const questions = [
+    {
+      question: "Which HTML tag is used to define an unordered list?",
+      options: ["<ol>", "<ul>", "<li>", "<list>"],
+      answer: 1
+    },
+    {
+      question: "What does CSS stand for?",
+      options: ["Cascading Style Sheets", "Creative Style Sheets", "Colorful Style Sheets", "Computer Style Sheets"],
+      answer: 0
+    },
+    {
+      question: "Which HTML tag is used to link an external JavaScript file?",
+      options: ["<script>", "<link>", "<js>", "<javascript>"],
+      answer: 0
+    },
+    {
+      question: "In CSS, which property is used to change the background color of an element?",
+      options: ["color", "background-color", "bg-color", "bgcolor"],
+      answer: 1
+    },
+    {
+      question: "What is the correct syntax for a JavaScript comment?",
+      options: ["<!-- This is a comment -->", "// This is a comment", "/* This is a comment */", "%% This is a comment %%"],
+      answer: 1
+    },
+    {
+      question: "Which HTML tag is used to define a table row?",
+      options: ["<row>", "<tr>", "<th>", "<td>"],
+      answer: 1
+    },
+    {
+      question: "In CSS, which property is used to change the text color of an element?",
+      options: ["text-color", "color", "font-color", "text-style"],
+      answer: 1
+    },
+    {
+      question: "Which JavaScript method is used to add an element to the end of an array?",
+      options: ["push()", "pop()", "shift()", "unshift()"],
+      answer: 0
+    },
+    {
+      question: "Which CSS property is used to create space between elements?",
+      options: ["margin", "padding", "space", "border"],
+      answer: 0
+    },
+    {
+      question: "Which HTML tag is used to define a hyperlink?",
+      options: ["<link>", "<a>", "<href>", "<hyperlink>"],
+      answer: 1
+    },
+    {
+      question: "In JavaScript, which loop is used to iterate over an array?",
+      options: ["for", "while", "do-while", "foreach"],
+      answer: 0
+    },
+    {
+      question: "What does the CSS property 'display: none;' do?",
+      options: ["Hides an element", "Shows an element", "Increases element visibility", "Resizes an element"],
+      answer: 0
+    },
+    {
+      question: "Which HTML tag is used to define the heading with the highest priority?",
+      options: ["<h1>", "<h2>", "<h3>", "<h4>"],
+      answer: 0
+    },
+    {
+      question: "Which operator is used for strict equality in JavaScript?",
+      options: ["==", "===", "!=", "!=="],
+      answer: 1
+    },
+    {
+      question: "In CSS, which property is used to specify the font size of an element?",
+      options: ["font-size", "text-size", "font-style", "text-style"],
+      answer: 0
+    }
+  ];
